2317f6fcee7e0d48a9702c7b4671330c88517350
Victor Langevin <2469827@cegepmontpetit.ca>
2026-04-10 15:21:08 -0400

    Changer le message pour dev, qa et prod

--- app/server.js
+++ app/server.js
@@ -4,9 +4,9 @@ const PORT = process.env.PORT || 3000;
 const ENV = process.env.APP_ENV || 'dev';
 
 const messages = {
-  dev: "Hello de l'environnement de DEV",
-  qa: "Hello de l'environnement de QA",
-  prod: "Hello de l'environnement de PROD",
+  dev: "Bonjour de l'environnement de DEV",
+  qa: "Bonjour de l'environnement de QA",
+  prod: "Bonjour de l'environnement de PROD",
 };
 
 const message = messages[ENV] || 'Hello World';
