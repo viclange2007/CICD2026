808919b2c3e87fb95f9da426495e07bd69f1f23f
Victor Langevin <2469827@cegepmontpetit.ca>
2026-04-10 17:10:43 -0400

    push prod

--- app/server.js
+++ app/server.js
@@ -6,7 +6,7 @@ const ENV = process.env.APP_ENV || 'dev';
 const messages = {
   dev: "Hello de l'environnement de DEV",
   qa: "Hello de l'environnement de QA",
-  prod: "Hello de l'environnement de PROD",
+  prod: "Hola de l'environnement de PROD",
 };
 
 const message = messages[ENV] || 'Hello World';
