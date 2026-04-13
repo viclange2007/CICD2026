93ff28da0713c07cbebb46914aff1df417735550
Victor Langevin <2469827@cegepmontpetit.ca>
2026-04-13 11:27:44 -0400

    Test dev

--- app/server.js
+++ app/server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 const ENV = process.env.APP_ENV || 'dev';
 
 const messages = {
-  dev: "Salut de l'environnement de DEV",
+  dev: "Salut de l'environnement de DEV pour correction",
   qa: "Bonjour de l'environnement de QA",
   prod: "Bonjour de l'environnement de PROD",
 };
